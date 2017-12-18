ba11cc9f4684402542ac321c57a6030221fadb4a
Matt Zhang <set.stun@gmail.com>
2017-12-18 16:07:44 -0600

    First commit

---
+++ OpenAIPackages.py
@@ -0,0 +1,21 @@
+import gym
+import matplotlib.pyplot as plt
+
+observations = []
+rewards = []
+
+def run(model, maxSteps=1000):
+    env = gym.make(model)
+    env.reset()
+    for step in range(maxSteps):
+        env.render()
+        observation, reward, done, info = env.step(env.action_space.sample())
+        observations.append(observation)
+        rewards.append(reward)
+        if done:
+            print("Finished after %d time steps.", step+1)
+            break
+
+def rewardHistory():
+    plt.plot(rewards)
+    plt.show()
